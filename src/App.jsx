895018ca36a6821a238efa0d53b9d657d9f1c111
Sai Preethi <saipreethidoodathi696@gmail.com>
2025-11-08 18:46:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import UserInfo from './UserInfo'
+import Button from './Button.jsx'
+
+export default function App() {
+  
+  return (
+    <div>
+      <h1>Hello, Redux!</h1>
+      <UserInfo />
+      <Button />
+    </div>
+  )
+}
+
